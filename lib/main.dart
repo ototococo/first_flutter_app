695a516008f72cf81a0ff52d32011e98ad238bc9
ototococo <ototo.coco@gmail.com>
2023-04-25 23:38:50 +0900

    textAlign: TextAlign.center 추가

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Text('Flutter Demo Home Page'),
+      home: Text('Flutter Demo Home Page',textAlign: TextAlign.center),
     );
   }
 }
